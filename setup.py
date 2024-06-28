ebad7134313e38d77192368fe3223fc55315d1bb
Abdo Mo <enay9911@gmail.com>
2024-06-28 12:09:45 +0300

    modifing the setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup ,find_packages
 
+from pkg_resources import parse_requirements
+
+with open('requirements.txt') as f:
+    requirements = [str(req) for req in parse_requirements(f)]
+
 setup(
    name='chatbot',
    version='1.0',
@@ -7,5 +12,6 @@ setup(
    author='Abdullah Jumaa',
    author_email='abdullah.m.jumaa@gmail.com',
    packages=find_packages(),  
+   install_requires=requirements,
 )
 
